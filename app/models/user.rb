c86a613bbb25962d9580ee3edc1f49eca9f70b53
EC2 Default User <ec2-user@ip-172-31-15-229.ap-northeast-1.compute.internal>
2024-10-21 11:44:28 +0000

    [Finish]UserMOdel/BookModel

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :image
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+        
+  has_many :books, dependent: :destroy
+  
+end
